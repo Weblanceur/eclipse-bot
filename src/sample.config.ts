0f1f146730a5f6d6a675a6e401e1a924ac3b2559
Андрей Акименко <akimenko@contact.work>
2022-10-22 22:50:03 +0300

    Типовой конфиг проекта

---
+++ src/sample.config.ts
@@ -0,0 +1,14 @@
+// rename this to app.config.ts
+export default {
+  main: {
+    port: 365,
+    secret: 'tghgtrgrdgd',
+  },
+  db: {
+    host: 'localhost',
+    port: 5432,
+    name: 'eclipse',
+    user: 'eclipse',
+    pass: 'lexmark',
+  },
+}
