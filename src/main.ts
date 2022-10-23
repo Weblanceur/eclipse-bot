b9c1423d90092e09505083d3302eab4ff1072ff9
Андрей Акименко <akimenko@contact.work>
2022-10-23 14:09:13 +0300

    SSL подключение

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 
 import AppConfig from './app.config'
+import * as fs from 'fs'
 
 async function bootstrap() {
   const PORT = AppConfig.main.port
-  const app = await NestFactory.create(AppModule)
+
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/ssl/private/ssl-cert-snakeoil.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/ssl-cert-snakeoil.pem'),
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions })
+
   app.enableCors({
     origin: ['http://localhost:5500'],
     credentials: true,
